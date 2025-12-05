0260a6bf92b6cf77f55e7c1f889db0cf3c701d8f
Pete Gamache <pete@gamache.org>
2025-12-05 09:57:54 -0500

    day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,7 +1,7 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Range {
     low: u64,
-    high: u64
+    high: u64,
 }
 impl From<&str> for Range {
     fn from(s: &str) -> Self {
@@ -20,16 +20,50 @@ impl Range {
 pub fn run(input: &String) {
     let mut splitter = input.splitn(2, "\n\n");
     let ranges: Vec<Range> = splitter.next().unwrap().lines().map(|l| l.into()).collect();
-    let ingredients: Vec<u64> = splitter.next().unwrap().lines().flat_map(|l| l.parse::<u64>()).collect();
-    let mut fresh1 = 0usize;
+    let ingredients: Vec<u64> = splitter
+        .next()
+        .unwrap()
+        .lines()
+        .flat_map(|l| l.parse::<u64>())
+        .collect();
+    part1(&ranges, &ingredients);
+    part2(&ranges);
+}
 
-    'ing_loop: for ingredient in &ingredients {
-        for range in &ranges {
+fn part1(ranges: &Vec<Range>, ingredients: &Vec<u64>) {
+    let mut fresh = 0usize;
+    'ing_loop: for ingredient in ingredients {
+        for range in ranges {
             if range.contains(*ingredient) {
-                fresh1 += 1;
+                fresh += 1;
                 continue 'ing_loop;
             }
         }
     }
-    println!("day 05 part 1: {}", fresh1);
+    println!("day 05 part 1: {}", fresh);
+}
+
+fn part2(ranges: &Vec<Range>) {
+    let mut rs = ranges.clone();
+    rs.sort_by(|a, b| a.low.cmp(&b.low));
+
+    let mut count = 0u64;
+    let mut cur_low = 0u64;
+    let mut cur_high = 0u64;
+
+    for range in rs {
+        if range.low > cur_high {
+            if cur_high > 0 {
+                count += cur_high - cur_low + 1;
+            }
+            cur_low = range.low;
+            cur_high = range.high;
+        } else {
+            if range.high > cur_high {
+                cur_high = range.high;
+            }
+        }
+    }
+    count += cur_high - cur_low + 1;
+    println!("day 05 part 2: {}", count);
 }
