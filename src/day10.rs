14dc1328417c635864f2062f579db84ab11e226e
Pete Gamache <pete@gamache.org>
2025-12-10 14:55:50 -0500

    day 10 part 2, incomplete

--- src/day10.rs
+++ src/day10.rs
@@ -3,6 +3,8 @@ use std::{
     collections::{BinaryHeap, HashSet},
 };
 
+use ndarray::prelude::*;
+use ndarray_linalg::Solve;
 use rayon::prelude::*;
 use regex::Regex;
 
@@ -10,33 +12,40 @@ pub fn run(lines: &Vec<String>) {
     let machines: Vec<Machine> = lines.iter().map(Machine::new).collect();
     let part1: usize = machines.par_iter().map(|m| m.minimum_presses_1()).sum();
     println!("day 10 part 1: {}", part1);
-    let part2: usize = machines.par_iter().map(|m| m.minimum_presses_2()).sum();
-    println!("day 10 part 2: {}", part2);
+    // let part2: usize = machines.par_iter().map(|m| m.minimum_presses_2()).sum();
+    // println!("day 10 part 2: {}", part2);
+
+    let a: Array2<f64> = array![
+        [1., 1., 1., 0.],
+        [1., 0., 1., 1.],
+        [1., 0., 1., 1.],
+        [1., 1., 0., 0.],
+        [1., 1., 1., 0.],
+        [0., 0., 1., 0.],
+    ];
+    let b: Array1<f64> = array![10., 11., 11., 5., 10., 5.];
+    let x = a.solve_into(b);
+    println!("{:?}", x);
 }
 
 #[derive(Debug)]
 struct Machine {
     lights: Vec<bool>,
-    goal: Vec<bool>,
     buttons: Vec<Vec<usize>>,
     joltages: Vec<u64>,
 }
 impl Machine {
     pub fn new(line: &String) -> Self {
-        let goal_re = Regex::new(r"[.#]").unwrap();
+        let lights_re = Regex::new(r"[.#]").unwrap();
         let button_re = Regex::new(r"\(([\d,]+)\)").unwrap();
         let joltages_re = Regex::new(r"\{([\d,]+)\}").unwrap();
 
-        let mut lights: Vec<bool> = vec![];
-        let goal: Vec<bool> = goal_re
+        let lights: Vec<bool> = lights_re
             .find_iter(line)
-            .map(|m| {
-                lights.push(false);
-                match m.as_str() {
-                    "." => false,
-                    "#" => true,
-                    _ => panic!("bad goal"),
-                }
+            .map(|m| match m.as_str() {
+                "." => false,
+                "#" => true,
+                _ => panic!("bad goal"),
             })
             .collect();
 
@@ -58,7 +67,6 @@ impl Machine {
 
         Self {
             lights: lights,
-            goal: goal,
             buttons: buttons,
             joltages: joltages,
         }
@@ -83,26 +91,22 @@ impl Machine {
     pub fn minimum_presses_1(&self) -> usize {
         let mut prev_lights: HashSet<Vec<bool>> = HashSet::new();
         let mut heap = BinaryHeap::new();
-        heap.push(SearchState {
+        heap.push(SearchState1 {
             presses: 0,
-            remaining_cost: 0,
             lights: self.lights.clone(),
-            joltages: vec![],
         });
 
         while let Some(ss) = heap.pop() {
-            if ss.lights == self.goal {
+            if ss.lights == self.lights {
                 return ss.presses as usize;
             }
             if prev_lights.contains(&ss.lights) {
                 continue;
             }
             for button in &self.buttons {
-                heap.push(SearchState {
+                heap.push(SearchState1 {
                     presses: ss.presses + 1,
-                    remaining_cost: 0,
                     lights: Self::apply_button_to_lights(&ss.lights, button),
-                    joltages: vec![],
                 })
             }
             prev_lights.insert(ss.lights);
@@ -119,53 +123,68 @@ impl Machine {
             empty_joltages.push(0);
         }
 
-        heap.push(SearchState {
+        heap.push(SearchState2 {
             presses: 0,
-            remaining_cost: 0,
-            lights: vec![],
             joltages: empty_joltages,
         });
 
         while let Some(ss) = heap.pop() {
-            if ss.joltages == self.joltages {
-                println!("{}", ss.presses);
-                return ss.presses as usize;
-            }
-            for i in 0..self.joltages.len() {
-                if ss.joltages[i] > self.joltages[i] {
+            for button in &self.buttons {
+                let new_joltages = Self::apply_button_to_joltages(&ss.joltages, button);
+
+                if prev_joltages.contains(&new_joltages) {
                     continue;
                 }
-            }
-            if prev_joltages.contains(&ss.joltages) {
-                continue;
-            }
-            for button in &self.buttons {
-                heap.push(SearchState {
+                prev_joltages.insert(new_joltages.clone());
+
+                for i in 0..self.joltages.len() {
+                    if new_joltages[i] > self.joltages[i] {
+                        continue;
+                    }
+                }
+
+                if new_joltages == self.joltages {
+                    println!("{}", 1 + ss.presses);
+                    return 1 + ss.presses as usize;
+                }
+
+                heap.push(SearchState2 {
                     presses: ss.presses + 1,
-                    remaining_cost: 0,
-                    lights: vec![],
-                    joltages: Self::apply_button_to_joltages(&ss.joltages, button),
+                    joltages: new_joltages,
                 })
             }
-            prev_joltages.insert(ss.joltages);
         }
         0
     }
 }
 
 #[derive(Debug, Eq, PartialEq)]
-struct SearchState {
+struct SearchState1 {
     presses: u64,
-    remaining_cost: u64,
     lights: Vec<bool>,
-    joltages: Vec<u64>,
 }
-impl Ord for SearchState {
+impl Ord for SearchState1 {
     fn cmp(&self, other: &Self) -> Ordering {
-        (other.presses + other.remaining_cost).cmp(&(self.presses + self.remaining_cost))
+        other.presses.cmp(&self.presses)
     }
 }
-impl PartialOrd for SearchState {
+impl PartialOrd for SearchState1 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct SearchState2 {
+    presses: u64,
+    joltages: Vec<u64>,
+}
+impl Ord for SearchState2 {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.presses.cmp(&self.presses)
+    }
+}
+impl PartialOrd for SearchState2 {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
